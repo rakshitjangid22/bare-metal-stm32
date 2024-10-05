f84fa3acc545705e565d00925e43306afc48e987
Rakshit Jangid <rakshitjangid22@gmail.com>
2024-10-05 19:26:59 +0530

    Bootloader_v3
    The most appealing and probably the best way to make it. Made use of the 'vector.h' header file. Used the concept of accessing a member of the struct using a pointer. Note that all the three asm_dump files have the same assembly instructions for the main function, hence confirming the fact that all the three versions of the bootloader are effectively the same.

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,24 +1,18 @@
 #include "common-defines.h"
 #include <libopencm3/stm32/memorymap.h>
+#include <libopencm3/cm3/vector.h> 
 
 #define BOOTLOADER_SIZE (0x10000U)     // 64KB(2^16)--->HEX
 #define MAIN_APP_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE)
 
 static void jump_to_main(void){
-  typedef void (*void_fn)(void);  // A function pointer
-
-  uint32_t* main_vector_table = (uint32_t*)(MAIN_APP_START_ADDRESS);
-  uint32_t* reset_vector_entry = &main_vector_table[1];  // Reset vector is the second entry in the vector table(each entry->4 bytes)
-  uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-
-  void_fn jump_fn = (void_fn)reset_vector;
-
-  jump_fn();
-
+  
+  vector_table_t* main_vector_table = (vector_table_t*)MAIN_APP_START_ADDRESS;
+  main_vector_table->reset();
 }
 
 int main(void){
-  
+
   jump_to_main();
   return 0;
 }
